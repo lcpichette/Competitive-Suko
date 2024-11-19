c4a7fca205f2abbcb5081ec5965d3e57012f3d71
Lucas Pichette <lucascpichette@gmail.com>
2024-11-19 09:42:01 -0800

    updates config

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,15 +7,13 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-    <body className={"min-h-screen max-h-screen min-w-screen max-w-screen bg-grain bg-white box-border"}>
-    <div className={"z-10 relative"}>
-      {children}
-    </div>
-    {
-    /*<div className="z-1 fixed -right-[25%] -bottom-[25%] w-[50vh] h-[50vh] md:w-[75vh] md:h-[75vh] pointer-events-none bg-gradient-to-tr from-white/60 to-amber-500/60 rounded-full blur-2xl"></div>*/
-    }
-    </body>
+      <body
+        className={
+          "min-h-screen max-h-screen min-w-screen max-w-screen bg-grain bg-white box-border"
+        }
+      >
+        <div className={"z-10 relative"}>{children}</div>
+      </body>
     </html>
   );
 }
-
